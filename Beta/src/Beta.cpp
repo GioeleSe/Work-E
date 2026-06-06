a83bcf49f2a29d88b4890ea494b33bf67b68c085
shrimperetti <windsgxela@gmail.com>
2026-06-06 09:35:16 +0200

    HAL integration: beginning

--- Beta/src/Beta.cpp
+++ Beta/src/Beta.cpp
@@ -1,93 +1,170 @@
 #include "Beta.h"
-#include "UDP_Client.h"
-#include "UDP_Server.h"
+#include "main_robot.h"
+#include "common_platform_abstr.h"
+#include "Adafruit_SSD1306"
+// #include "UDP_Client.h"
+// #include "UDP_Server.h"
 
-// TODO
-// loadPayload()
-// dischargePayload()
-// radar
-// buzzer
-// LED ?
-//? the motor driver also has a FAULT detection pin
+// TODO: Low to High abstraction functions
+// + define robot public properties with getter and setter
+// - choose the reset and stop callbacks
+// - move motor with timeout, direction and speed
+// - move servo motor in the specific radar angle
+// - read the distance in a matrix with tuples (angle, distance)
+//
+// - define a radar function to populate the matrix with a single command
+// - move the trunk until the closed-button is active (openTrunk, closeTrunk and isOpenTrunk functions)
+// -
+//
+// - use LEDs for low-level debugging (1 for power and 1 for state, according to frequency can be good, bad or error)
+// - use OLED for high level debugging
+// - activate the buzzer with a nice tone when using trunk
 
-/*
-* DC MOTOR LOGIC:
+// properties defined in Beta.h, here only getter and setter
+extern Beta_t robot_beta;
+// extern int SELF_ROBOT_ID  // defined with its directive
+extern RobotState_t     robot_state;
+extern int              speed;
+extern int              feedback;
+extern int              debug;
+extern NavigationType_t navigation_type;
+extern RoutePolicy_t    route_policy;
+extern int              radar;
+extern int              screen;
+extern int              obstacle_cleaner;
+extern int              object_loader;
+extern int              object_unloader;
+extern int              object_compacter;
 
-* IN1 pin | IN2 pin |   Direction
-* ----------------------------------------------------
-*   LOW   |   LOW   |     stop
-*   HIGH  |  HIGH   |     stop
-*   HIGH  |   LOW   |   forward (spin clockwise)
-*   LOW   |  HIGH   |   reverse (spin anti-clockwise)
-*/
+extern const char* logSeverityLabels;
+extern Adafruit_SSD1306 display;
 
-// Spin the motors!
-/// \param dutyCycle  Percentage of the duty cycle to be used to set PWM (0-100%)
-/// \param dir  Direction of movement
-//!! TO BE CHANGED:
-//!! move only forwards and backwards, turn to change directions
-//!!     stop()
-//!!     go()
-//!!     turn(LEFT | RIGHT)
-void move(int dutyCycle, Direction dir)
-{
-    int speed = (int)(MAX_SPEED * dutyCycle);
+int self_prop_get_robot_id() { return SELF_ROBOT_ID; }
+int self_prop_get_robot_state() { return (int)robot_state; }
+int self_prop_get_speed() { return speed; }
+int self_prop_get_feedback() { return feedback; }
+int self_prop_get_debug() { return debug; }
+int self_prop_get_navigation_type() { return (int)navigation_type; }
+int self_prop_get_route_policy() { return (int)route_policy; }
+int self_prop_get_radar() { return radar; }
+int self_prop_get_screen() { return screen; }
+int self_prop_get_obstacle_cleaner() { return obstacle_cleaner; }
+int self_prop_get_object_loader() { return object_loader; }
+int self_prop_get_object_unloader() { return object_unloader; }
+int self_prop_get_object_compacter() { return object_compacter; }
 
-    //? if case con uno switch dentro? huh???
+int self_prop_set_robot_state(RobotState_t val) { robot_state = val; }
+int self_prop_set_speed(int val) { speed = val; }
+int self_prop_set_feedback(int val) { feedback = val; }
+int self_prop_set_debug(int val) { debug = val; }
+int self_prop_set_navigation_type(NavigationType_t val) { navigation_type = val; }
+int self_prop_set_route_policy(RoutePolicy_t val) { route_policy = val; }
+int self_prop_set_radar(int val) { radar = val; }
+int self_prop_set_screen(int val) { screen = val; }
+int self_prop_set_obstacle_cleaner(int val) { obstacle_cleaner = val; }
+int self_prop_set_object_loader(int val) { object_loader = val; }
+int self_prop_set_object_unloader(int val) { object_unloader = val; }
+int self_prop_set_object_compacter(int val) { object_compacter = val; }
 
-    switch (dir)
-    {
-    FORWARD:
-        spinClockwise(MOTOR_1A_PIN, MOTOR_1B_PIN, speed);
-        spinClockwise(MOTOR_2B_PIN, MOTOR_2B_PIN, speed);
-        // self.moving = true;
-        break;
-    BACKWARD:
-        spinAntiClockwise(MOTOR_1A_PIN, MOTOR_1B_PIN, speed);
-        spinAntiClockwise(MOTOR_2B_PIN, MOTOR_2B_PIN, speed);
-        // self.moving = true;
-        break;
-    LEFT:
-        // todo turn()
-        //  self.moving = true;
-        break;
-    RIGHT:
-        // todo turn()
-        //  self.moving = true;
-        break;
-    STOP:
-        digitalWrite(ULT_PIN, LOW);
-        // stop();
-        break;
-    default:
-        //!! ERROR
-        //!! handle it NOW!!
-        break;
+// messages printing functions
+void logMessage(ErrorSeverity_t severity, const char* message){
+#ifdef SERIAL_PRINT
+    Serial.print("[");
+    Serial.print(logSeverityLabels[severity]);
+    Serial.print("] ");
+    Serial.println(message);
+#endif
+}
+
+void oledInit() {
+    Wire.begin(PIN_OLED_SDA, PIN_OLED_SCL);
+    Wire.setClock(400000);  // 400kHz refresh for Oled responsiveness
+   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+        Serial.println("SSD1306 allocation failed");
+        robot_beta.screen = 0;  // screen state update (failed -> force status to 0)
+        return;
+    }
+    
+    display.clearDisplay();
+    display.display();
+    robot_beta.screen = 1;
+}
+
+void oledPrint(const char* message){
+    if (robot_beta.screen == 0) return; 
+
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.print(message);
+    display.display();
+}
+
+// direct reset with log attempt only, not the gentle one
+int self_hard_reset(){
+    logMessage(ErrorSeverity_t::ErrorSeverity_t_HIGH, "Hard Reset...");
+    ESP.restart();
+    return 0;
+}
+
+void rebootTimerCallback(TimerHandle_t xTimer) {
+    logMessage(ErrorSeverity_t::ErrorSeverity_t_HIGH, "Timer expired. Executing Hard Reset...");
+    ESP.restart();
+}
+
+// warn the tasks that the reboot is going to happen
+int self_soft_reset() {
+    logMessage(ErrorSeverity_t::ErrorSeverity_t_HIGH, "Soft reset initiated");
+    if(robot_beta.isRequiredStop){
+        logMessage(ErrorSeverity_t::ErrorSeverity_t_LOW, "Soft reset - Emergency Stop deactivated");
+        oledPrint("Emergency Stop cleared");
+        return 0;
+    }
+
+    robot_beta.isRequiredReset = 1;
+    oledPrint("Soft Reset in 2s");
+
+    TimerHandle_t rebootTimer = xTimerCreate(
+        "RebootTimer", 
+        pdMS_TO_TICKS(2000), 
+        pdFALSE,    // run one time only
+        (void*)0,   // timer id set to 0
+        rebootTimerCallback
+    );
+
+    if (rebootTimer != NULL) {
+        xTimerStart(rebootTimer, 0);
+        logMessage(ErrorSeverity_t::ErrorSeverity_t_LOW, "Reboot scheduled in 2 seconds.");
+        return 0;
+    } else {
+        logMessage(ErrorSeverity_t::ErrorSeverity_t_HIGH, "Timer creation failed! Forcing hard reset."); // fallback for freertos timer failure
+        ESP.restart(); 
+        return -1;  // not really needed as it's restarting
     }
 }
 
-void spinClockwise(int MOTOR_PIN1, int MOTOR_PIN2, int speed)
-{
-    digitalWrite(ULT_PIN, HIGH);
-    analogWrite(MOTOR_PIN1, speed);
-    digitalWrite(MOTOR_PIN2, LOW);
+// set the stop flag to warn the motor-controlling functions
+int self_emergency_stop(){
+    robot_beta.isRequiredStop = 1;      // will be cleared with soft reset, the motor movements will be prevented by this flag 
+    logMessage(ErrorSeverity_t::ErrorSeverity_t_MID, "Emergency Stop required");
+    oledPrint("Emergency Stop");
 }
 
-void spinAntiClockwise(int MOTOR_PIN1, int MOTOR_PIN2, int speed)
-{
-    digitalWrite(ULT_PIN, HIGH);
-    digitalWrite(MOTOR_PIN1, LOW);
-    analogWrite(MOTOR_PIN2, speed);
-}
 
-/// Stops all motors
-//?? set low power mode for motors -> here or in main?
-void stop()
-{
-    analogWrite(MOTOR_1A_PIN, 0);
-    analogWrite(MOTOR_1B_PIN, 0);
-    analogWrite(MOTOR_2A_PIN, 0);
-    analogWrite(MOTOR_2B_PIN, 0);
+// dc and servo motors control
+/*
+ * DC MOTOR LOGIC:
+ *   IN_1 IN_2 -> Direction
+ *   Both LOW -> stop
+ *   Both HIGH -> stop
+ *   LOW HIGH -> clockwise
+ *   HIGH LOW -> counterclockwise
+ */
 
-    // self.moving = false;
-}
+// int self_motion_activate_dc_motor(Motors_t motor_id, Direction_t direction, int speed, int duration);
+// int self_motion_stop_motor(Motors_t motor_id);
+// int self_motion_steer_servo(Motors_t motor_id, int angle);
+// int self_motion_car_rotate(Direction_t direction);
+// int self_motion_car_proceed(Direction_t direction);
+// int self_motion_car_stop();
--- Beta/src/Beta.h
+++ Beta/src/Beta.h
@@ -1,74 +1,28 @@
-// Header file for defining the Software/Hardware interface and controlling the robot
+#include <Wire.h>
+#ifndef BETA_H // Header file for defining the Software/Hardware interface and controlling the robot
 
-#ifndef BETA
+#define BETA_H
 
-#define BETA
-
-using namespace std;
-
-#define ROBOT_ID 1
-
-// Control pins for motors 1-2
-//?? placeholders
-#define MOTOR_1A_PIN 0
-#define MOTOR_1B_PIN 1
-#define MOTOR_2A_PIN 2
-#define MOTOR_2B_PIN 3
-#define ULT_PIN 4 // To enable sleep mode for the motors
-
+#define SERIAL_BAUD 115200  // serial speed here for easier adjustments
+#define SELF_ROBOT_ID 3     // ID assigned only here to make usage easier
 #define MAX_SPEED 2.55
 
-//!!! remake all these according to protocol !!!
-typedef enum RobotState
-{
-    RobotState_IDLE = 0,
-    RobotState_BUSY = 1,
-    RobotState_ERROR = 2,
-} RobotState;
+// Control pins for motors 1-2 (Car movements) and trunk
+#define PIN_DRIVER_1_MOTOR_1A 25
+#define PIN_DRIVER_1_MOTOR_1B 23
+#define PIN_DRIVER_1_MOTOR_2A 22
+#define PIN_DRIVER_1_MOTOR_2B 21
+#define PIN_DRIVER_1_SLEEP 16
 
-typedef enum ActionResult
-{
-    ActionResult_SUCCESS = 0,
-    ActionResult_FAILURE = 1,
-    ActionResult_PENDING = 2,
-} ActionResult;
+#define PIN_DRIVER_2_MOTOR_1A 25
+#define PIN_DRIVER_2_MOTOR_1B 23
+#define PIN_DRIVER_2_SLEEP 17
 
-typedef enum FeedbackEvent
-{
-    OBSTACLE_DETECTED = 10,
-    OBSTACLE_REMOVED = 11,
-    POI_REACHED = 12,
-    LOAD_COLLECTED = 13,
-    LOAD_DISPOSED = 14,
-    REROUTE_REQUIRED = 15,
-    MISSING_LOAD = 16
-} FeedbackEvent;
-
-typedef enum ErrorSeverity
-{
-    ErrorSeverity_LOW = 0,
-    ErrorSeverity_MID = 1,
-    ErrorSeverity_HIGH = 2,
-} ErrorSeverity;
-
-typedef enum NavigationType
-{
-    NavigationType_MANUAL = 0, // manual GUI, robot as dummy collection of motors
-    NavigationType_CHECKPOINT = 1, // home-load-target navigation
-    NavigationType_GRID = 2, // prefixed grid pattern (90° rotations only at turning points, like a chess board)
-    NavigationType_FREE_MOVE = 3 // no target, no grid, just an happy robot going around (for future extensions obv)
-} NavigationType;
-
-typedef enum Direction
-{
-    Direction_FORWARD = 0,
-    Direction_BACKWARD = 1,
-    Direction_LEFT = 2,
-    Direction_RIGHT = 3,
-    Direction_STOP = 4,
-} Direction;
-
-typedef enum MotorID
+const int MOTOR_MAPPING_CAR_LEFT = 1;
+const int MOTOR_MAPPING_CAR_RIGHT = 2;
+const int MOTOR_MAPPING_TRUNK = 3;
+const int MOTOR_MAPPING_RADAR = 4;
+typedef enum
 {
     MotorID_END_MOT = -1,
     MotorID_RES = 0,
@@ -76,43 +30,55 @@ typedef enum MotorID
     MotorID_MOT2 = 2, // Wheel DC motor 2
     MotorID_MOT3 = 3, // Trunk DC motor
     MotorID_MOT4 = 4, // Radar Servo motor
-} MotorID;
+} MotorRobotToID;
 
-//!! you may NOT need all of these
-typedef enum Properties
-{
-    Properties_SPEED = 0,       // slow | normal | fast
-    Properties_FEEDBACK = 1,    // silent | minimal | debug
-    Properties_DEBUG = 2,           // off | basic | full
-    Properties_NAVIGATION_TYPE = 3, // manual | checkpoint | grid | free_move
-    Properties_ROUTE_POLICY = 4,    // shortest | safest | fast
-    Properties_RADAR = 5,           // 0 | 1 (OFF | ON)
-    Properties_SCREEN = 6,          // 0 | 1
-    Properties_OBJECT_LOADER = 7,   // 0 | 1
-    Properties_OBJECT_UNLOADER = 8, // 0 | 1
-} Properties;
+// serial and screen properties and defintions
+#define SERIAL_PRINT
+#define PIN_OLED_SDA 33
+#define PIN_OLED_SCL 32
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET    -1 
+#define SCREEN_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-//** 0 = OK
-typedef int errorCode_t;
 
-typedef struct Beta
-{
-    NavigationType navigationMode; //!! NO
-    RobotState currentState;
-    Properties *properties;
+
+struct Beta_t{
+    RobotState_t robot_state = RobotState_t::RobotState_IDLE;
+    int speed = 0;
+    int feedback = 0;
+    int debug = 0;
+    NavigationType_t navigation_type = NavigationType_t::NavigationType_MANUAL;
+    RoutePolicy_t route_policy = RoutePolicy_t::RoutePolicy_FAST;
+    int radar = 0;
+    int screen = 0;
+    int obstacle_cleaner = 0;
+    int object_loader = 0;
+    int object_unloader = 0;
+    int object_compacter = 0;
+
     ActionResult lastActionResult;
     // Booleans to be used to determine currentState
-    // bool connected;
-    // bool moving;
-} Beta;
+    int isRequiredReset;    // used for soft reset (stop actions before rebooting the board)
+    int isRequiredStop;     // used for emergency stop
+    int isServerConnected;
+    int isCarMoving;
+    int isRadarMoving;
+    int isOpenTrunk;
+    int isBuzzerPlaying;
+} robot_beta;   // defined and initialized only here
 
-void moveForward();
-void moveBackward();
-void stop();
 
-//!! very high-level, it would be better to work terra terra first
-void move(int dutyCycle, Direction dir);
-void spinClockwise(int MOTOR_PIN1, int MOTOR_PIN2, int speed);
-void spinAntiClockwise(int MOTOR_PIN1, int MOTOR_PIN2, int speed);
+const char* logSeverityLabels[] = {
+    "INFO",
+    "WARN",
+    "ERROR"
+};
 
+void logMessage(ErrorSeverity_t severity, const char* message);
+void oledInit();    // call this in the setup before usage
+void oledPrint(const char* message);
+
+void rebootTimerCallback(TimerHandle_t xTimer);
 #endif
